464a0dd04f88ae1fb2ae56fc0b3b6d35f5fa3a1e
gabrypol <gabrielepolenta@gmail.com>
2019-10-08 02:42:09 +0200

    Updates

---
+++ precache-manifest.4d551ab7e0ed826ce711a2383139de66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0f0b4b958adf62ba4c2",
+    "url": "/dictionary-management-application/static/js/runtime~main.d0f0b4b9.js"
+  },
+  {
+    "revision": "1e443d359666bd485cfc",
+    "url": "/dictionary-management-application/static/js/main.1e443d35.chunk.js"
+  },
+  {
+    "revision": "4b36abc51f67c8a84da6",
+    "url": "/dictionary-management-application/static/js/1.4b36abc5.chunk.js"
+  },
+  {
+    "revision": "1e443d359666bd485cfc",
+    "url": "/dictionary-management-application/static/css/main.f90e1154.chunk.css"
+  },
+  {
+    "revision": "e1aa8b9d4294a2e33e84e99b72b3d78d",
+    "url": "/dictionary-management-application/index.html"
+  }
+];
